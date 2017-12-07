ea589668e5b5706093c33a5def83468ce575abff
KangarooFrog <l85a2556@gmail.com>
2017-12-07 18:58:13 +0900

    no message

--- Pracitce02.java
+++ Pracitce02.java
@@ -8,11 +8,15 @@ public class Pracitce02 {
 
 		boolean ret1 = false;
 		boolean ret2 = false;
+		int count =0;
 		do {
+
 			System.out.println("メインdoから出力");
 			ret1 = whileTest.test();
 			ret2 = whileTest.test2();
-		}while(!ret1 && !ret2) ;
+			count++;
+			if(count == 4)break;
+		}while(ret1 && ret2) ;
 
 	}
 }
